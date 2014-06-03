310c5fd7ce86b5e5ce0255ac525eac340339b7b0
hoaht <hoaht_55@vnu.edu.vn>
2014-06-03 17:58:43 +0700

    demo

--- Project Managerment/Project Managerment/Classes/Global/App Delegate/QSAppDelegate.m
+++ Project Managerment/Project Managerment/Classes/Global/App Delegate/QSAppDelegate.m
@@ -17,6 +17,9 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
+    
+    
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
