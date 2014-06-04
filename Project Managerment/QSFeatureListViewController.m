500704165da969fc423215572566ba33f1450667
huuhieu <duonghuuhieu@mhtranbns-MacBook-Pro.local>
2014-06-04 16:57:03 +0700

    Update Feature List UI

---
+++ Project Managerment/QSFeatureListViewController.m
@@ -0,0 +1,38 @@
+//
+//  QSFeatureListViewController.m
+//  Project Managerment
+//
+//  Created by Duong Huu Hieu on 6/4/14.
+//  Copyright (c) 2014 Cừu Lười. All rights reserved.
+//
+
+#import "QSFeatureListViewController.h"
+
+@interface QSFeatureListViewController ()
+
+@end
+
+@implementation QSFeatureListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
