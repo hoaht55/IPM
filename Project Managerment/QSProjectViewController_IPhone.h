87c9ea0f72d3561d60eefb1d7227766a608cbc6c
MinhTien <nguyenminhtien.hust@gmail.com>
2014-06-05 14:04:05 +0700

    iPad and iPhone done 80%

---
+++ Project Managerment/QSProjectViewController_IPhone.h
@@ -0,0 +1,13 @@
+//
+//  QSProjectViewController_IPhone.h
+//  Project Managerment
+//
+//  Created by MinhTien on 6/5/14.
+//  Copyright (c) 2014 Cừu Lười. All rights reserved.
+//
+
+#import "QSProjectViewController.h"
+
+@interface QSProjectViewController_IPhone : QSProjectViewController
+
+@end
---
+++ Project Managerment/QSRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  QSRootViewController.h
+//  Project Managerment
+//
+//  Created by MinhTien on 6/5/14.
+//  Copyright (c) 2014 Cừu Lười. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSRootViewController : UIViewController
+
+@end
